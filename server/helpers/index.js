5edebe85db2747a62ab1ae73c79266a09c65da6d
Adekoreday <adeyemi_adekorede@hotmail.com>
2020-09-26 18:21:26 +0100

    add get all pages with pagination

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -2,11 +2,14 @@ import generateToken from './generateToken';
 import { serverResponse, serverError } from './serverResponse';
 import emptyBody from './emptyBody';
 import sendMail from './sendMail';
+import { paginationValues, pageCounter } from './pagination';
 
 export {
-    generateToken,
-    serverResponse,
-    serverError,
-    emptyBody,
-    sendMail
-}
+  generateToken,
+  serverResponse,
+  serverError,
+  emptyBody,
+  sendMail,
+  paginationValues,
+  pageCounter
+};

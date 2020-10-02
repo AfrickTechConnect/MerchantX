35f6f13e5554e5a573067e6a72766dabeeab1da3
Adekoreday <adeyemi_adekorede@hotmail.com>
2020-10-02 19:34:30 +0100

    explore posts

--- frontEnd/src/components/post/index.jsx
+++ frontEnd/src/components/post/index.jsx
@@ -4,16 +4,29 @@ import CommentImg from "../../assets/svg/comment.svg"
 import "./index.css"
 
 const Post = (props) => {
-  const { title, description, Comments } = props.post
+  const { type } = props
+  const { title, description, Comments, Author } = props.post
   const totalComments = Comments.length
   return (
     <Fragment key={1}>
       <div className="post">
         <h3 className="post__title">{title}</h3>
-        {description}
-        <div className="post__comment">
-          <span className="comments__count">{totalComments} </span>
-          <Image className="comments__image" src={CommentImg} size="15px" />
+        <div className="descritpion__post">{description}</div>
+        <div className="post__subtext">
+          {type === "explore" ? (
+            <div className="post__user">
+              <Image rounded="full" size="20px" src={Author.avatarUrl} alt="username of author" />
+              <span className="post__author--name">
+                {Author.firstname.toUpperCase()} {Author.lastname.toUpperCase()}
+              </span>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="post__comment">
+            <span className="comments__count">{totalComments} </span>
+            <Image className="comments__image" src={CommentImg} size="15px" />
+          </div>
         </div>
       </div>
     </Fragment>
